e55a63b7b7e42d8c235bac76c5973b0c38ba78ef
elio <roloneliotaiel2002@gmail.com>
2025-07-01 15:46:27 -0300

    17 plus

---
+++ TP17.html/script/script3.js
@@ -0,0 +1,24 @@
+console.log("Ping script3.js");
+
+// tipos de datos 
+
+let numero = 7;
+let texto = "texto en js";
+let booleano = true; // false
+let nulo = null;
+let indefinido;
+let array = ["texto", 7, false, null, undefined, 8];
+let objeto = {
+    edad: 23, 
+    nombre: "elio",
+    estado: true,
+};
+
+console.log(numero);
+console.log(typeof(numero));
+console.log(typeof(string));
+console.log(typeof(booleano));
+console.log(typeof(nulo));
+console.log(typeof(indefinido));
+console.log(typeof(array));
+console.log(typeof(objeto));
