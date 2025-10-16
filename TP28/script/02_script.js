7139c53cb2c4270f9c196a994e29f53bd9a67420
elio <roloneliotaiel2002@gmail.com>
2025-10-16 16:00:48 -0300

    tp28

--- TP28/script/02_script.js
+++ TP28/script/02_script.js
@@ -1,60 +1,69 @@
 let Tareas = [];
 
 document.querySelector("#btnAgregar").addEventListener("click", () => {
-let tarea = document.querySelector("#tarea");
-// validar input
-if(tarea.value === ""){return}
-// crear objeto "tarea"
-let objTarea = {
-    tarea: tarea.value,
-    estado: "pendiente",
-    id: Tareas.length,
-}
-// agregar objeto al array global
-Tareas.push(objTarea);
-localStorage.setItem("tareas", JSON.stringify(Tareas));
-//limpiar pagina y mostrar datos
-tarea.value = "";
+    let tarea = document.querySelector("#tarea");
+    // validar input
+    if (tarea.value === "") { return }
+    // crear objeto "tarea"
+    let objTarea = {
+        tarea: tarea.value,
+        estado: "pendiente",
+        id: Tareas.length,
+    }
+    // agregar objeto al array global
+    Tareas.push(objTarea);
+    localStorage.setItem("tareas", JSON.stringify(Tareas));
+    //limpiar pagina y mostrar datos
+    tarea.value = "";
     // limpiar tbody
-    document.querySelector("tbody").innerHTML = "";
-Tareas.forEach((tarea) => {
-    //console.log(tarea);
-    //cargar info actualizada
-    document.querySelector("tbody").innerHTML += `<tr>
-                    <td>${tarea.id}</td>
-                    <td>${tarea.tarea}</td>
-                    <td>
-                        <div class="btn verde">${tarea.estado}</div>
-                    </td>
-                    <td>
-                        <div class="btn azul">editar</div>
-                        <div class="btn naranja">borrar</div>
-                    </td>
-                </tr>`;
-})
 });
 
-function buscarDatosPrevios(){
-    let TareasEnLS = localStorage.getItem("Tareas");
+function buscarDatosPrevios() {
+    let TareasEnLS = localStorage.getItem("tareas");
     //console.log(TareasEnLS);
     TareasEnLS = JSON.parse(TareasEnLS) || [];
-
-        document.querySelector("tbody").innerHTML = "";
-        TareasEnLS.forEach((tarea) => {
-    //console.log(tarea);
-    //cargar info actualizada
-    document.querySelector("tbody").innerHTML += `<tr>
-                    <td>${tarea.id}</td>
-                    <td>${tarea.tarea}</td>
-                    <td>
-                        <div class="btn verde">${tarea.estado}</div>
-                    </td>
-                    <td>
-                        <div class="btn azul">editar</div>
-                        <div class="btn naranja">borrar</div>
-                    </td>
-                </tr>`;
-})
+    // pasarlo como variable global
+    Tareas = TareasEnLS;
+    crearVista();
 }
 
 buscarDatosPrevios();
+
+function crearVista() {
+    document.querySelector("tbody").innerHTML = "";
+    Tareas.forEach((tarea) => {
+        if(tarea.estado == "eliminada") return
+        //cargar info actualizada
+        document.querySelector("tbody").innerHTML += `<tr>
+                    <td>${tarea.id}</td>
+                    <td>${tarea.tarea}</td>
+                    <td>
+                        <div class="btn verde btnEstado" data-id=${tarea.id} > ${tarea.estado}</div>
+                    </td>
+                    <td>
+                        <div class="btn azul btnEditar">editar</div>
+                         <div class="btn naranja btnBorrar data-id=${tarea.estado}">borrar</div>
+                     </td>
+                 </tr>`;
+
+    })
+    document.querySelectorAll(".btnEstado").forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+            if (Tareas[e.target.dataset.id].estado == "completa") { Tareas[e.target.dataset.id].estado = "Pendiente" } else { Tareas[e.target.dataset.id].estado = "completa" }
+            console.log(Tareas[e.target.dataset.id]);
+            localStorage.setItem("tareas", JSON.stringify(Tareas));
+
+            document.querySelectorAll(".btnBorrar").forEach((btn) => {
+                btn.addEventListener("click", (e) => {
+                    (Tareas[e.target.dataset.id].estado == "eliminada")
+
+
+                    crearVista();
+                })
+            }
+
+            )
+        });
+    });
+};
+
